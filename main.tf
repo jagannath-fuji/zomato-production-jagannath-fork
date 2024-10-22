24b2de61b9b9fe25601a7f89520add49860f732c
Fuji Komalan <fujikomalan@gmail.com>
2024-10-22 04:38:48 +0000

    Frontend Security Group Created

--- main.tf
+++ main.tf
@@ -8,3 +8,21 @@ resource "aws_key_pair" "ssh_keypair" {
   }
 }
 
+# Creating Frontend Security group
+resource "aws_security_group" "frontend" {
+
+  name        = "${var.project_name}-${var.project_environment}-frontend"
+  description = "${var.project_name}-${var.project_environment}-frontend"
+  vpc_id      = var.default_vpc_id
+  tags = {
+    Name = "${var.project_name}-${var.project_environment}-frontend"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
